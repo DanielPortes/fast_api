2e377721ea22711287014c01e36a4c7a6703a136
portes <fagundes.daniel191@gmail.com>
2024-09-04 13:56:18 -0300

    cria testes

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from fastapi.testclient import TestClient
+from fast_api.app import app
+
+@pytest.fixture(autouse=True)
+def client():
+    return TestClient(app)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,20 @@
-from fastapi.testclient import TestClient
-from fast_api.app import app
+from http import HTTPStatus
 
-client = TestClient(app)
+def test_read_root_deve_retornar_mensagem_hello_world(client):
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello, World!'}
+
+
+def test_create_user_deve_retornar_usuario_criado(client):
+    response = client.post('/users/', json={
+        'username': 'test',
+        'email': 'teste@teste.com',
+        'password': '123456',
+    })
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'test',
+        'email': 'teste@teste.com',
+        'id': 1,
+    }
