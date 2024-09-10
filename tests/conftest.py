bf0214a339ee1238b2acea30499de27e7ad20321
portes <fagundes.daniel191@gmail.com>
2024-09-09 23:45:31 -0300

    cria session para database

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,19 +2,33 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fast_api.app import app
-from fast_api.models import mapper_registry
+from fast_api.database import get_session
+from fast_api.models import mapper_registry, User
+from fast_api.security import get_password_hash
 
 
 @pytest.fixture()
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
-@pytest.fixture()
+@pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     mapper_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
