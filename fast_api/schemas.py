711bc75987857d3ecb072157b564eef366c3d401
portes <fagundes.daniel191@gmail.com>
2024-09-11 21:49:51 -0300

    criando schemas.py do todo

--- fast_api/schemas.py
+++ fast_api/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
+from fast_api.models import TodoState
 
 
 class Message(BaseModel):
@@ -25,4 +26,27 @@ class UserList(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
-    model_config = ConfigDict(from_attributes=True)
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+    state: TodoState
+
+
+class TodoPublic(TodoSchema):
+    id: int
+
+
+class TodoList(BaseModel):
+    todos: list[TodoPublic]
+
+
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    state: TodoState | None = None
