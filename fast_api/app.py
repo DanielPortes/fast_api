f804aeb54835e27a6abac8abb0fa0041d2b3d115
portes <fagundes.daniel191@gmail.com>
2024-09-11 21:54:27 -0300

    adiciona nova rota todo

--- fast_api/app.py
+++ fast_api/app.py
@@ -4,12 +4,14 @@ from http import HTTPStatus
 from fast_api.schemas import (
     Message,
 )
-from fast_api.routes import auth, users
+from fast_api.routes import auth, users, todo
+
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todo.router)
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
-
