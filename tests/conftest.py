1609dc869505464424c5453f0d8a72b9cccd4bd9
portes <fagundes.daniel191@gmail.com>
2024-09-13 16:03:06 -0300

    implementando Docker ao projeto

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,15 @@
 import pytest
+
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.pool import StaticPool
 
 from fast_api.app import app
 from fast_api.database import get_session
-from fast_api.models import mapper_registry, User
+from tests.factories import UserFactory
+from fast_api.models import mapper_registry
 from fast_api.security import get_password_hash
+from testcontainers.postgres import PostgresContainer
 
 
 @pytest.fixture()
@@ -23,26 +25,29 @@ def client(session):
 
 
 @pytest.fixture
-def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+def session(engine):
     mapper_registry.metadata.create_all(engine)
-
     with Session(engine) as session:
         yield session
+        session.rollback()
 
     mapper_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:
+        _engine = create_engine(postgres.get_connection_url())
+
+        with _engine.begin():
+            yield _engine
 
 
 @pytest.fixture
 def user(session):
     pwd = 'testtest'
-    user = User(
-        username='Teste', email='teste@test.com', password=get_password_hash(pwd)
-    )
+
+    user = UserFactory(password=get_password_hash(pwd))
 
     session.add(user)
     session.commit()
@@ -50,3 +55,30 @@ def user(session):
 
     user.clean_password = pwd  # monkey patching
     return user
+
+
+@pytest.fixture
+def other_user(session):
+    pwd = 'testtest'
+
+    user = UserFactory(password=get_password_hash(pwd))
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    user.clean_password = pwd
+
+    return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/auth/token',
+        data={
+            'username': user.email,
+            'password': user.clean_password,
+        },
+    )
+    return response.json()['access_token']
