e5f07b422e8db87980f0b33bc9673e3c660127d3
portes <fagundes.daniel191@gmail.com>
2024-09-12 22:39:40 -0300

    refatora endpoints de users.py

--- fast_api/routes/users.py
+++ fast_api/routes/users.py
@@ -19,16 +19,13 @@ from fast_api.schemas import (
 router = APIRouter()
 
 router = APIRouter(
-    prefix="/users",
-    tags=["users"],
+    prefix='/users',
+    tags=['users'],
 )
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(
-        user: UserSchema,
-        session: Session = Depends(get_session)
-):
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
     db_user = session.scalar(
         select(User).where(
             (User.username == user.username) | (User.email == user.email)
@@ -64,12 +61,22 @@ def read_users(limit: int = 10, skip: int = 0, session: Session = Depends(get_se
     return {'users': users}
 
 
+@router.get('/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    return db_user
+
+
 @router.put('/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
 def update_user(
-        user_id: int,
-        user: UserSchema,
-        session: Session = Depends(get_session),
-        current_user: User = Depends(get_current_user),
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
 ):
     if current_user.id != user_id:
         raise HTTPException(
@@ -84,25 +91,17 @@ def update_user(
 
 
 @router.delete('/{user_id}', response_model=Message)
-def delete_user(user_id: int,
-                session: Session = Depends(get_session),
-                current_user: User = Depends(get_current_user)):
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
     if current_user.id != user_id:
         raise HTTPException(
             status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
         )
-
     session.delete(current_user)
+
     session.commit()
 
     return {'message': 'User deleted'}
-
-
-@router.get('/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
-def read_user(user_id: int, session: Session = Depends(get_session)):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-
-    if not db_user:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
-
-    return db_user
