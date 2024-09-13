678a16068d8130440a41e014b0830d44d7a578dc
portes <fagundes.daniel191@gmail.com>
2024-09-12 22:38:51 -0300

    refatora auth

--- fast_api/routes/auth.py
+++ fast_api/routes/auth.py
@@ -6,21 +6,29 @@ from http import HTTPStatus
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
-from fast_api.security import create_access_token, verify_password
+
+from fast_api.schemas import Token
+from fast_api.security import create_access_token, verify_password, get_current_user
 
 from fast_api.database import get_session
 from fast_api.models import User
 
+from typing import Annotated
 
 router = APIRouter(
-    prefix="/auth",
-    tags=["auth"],
+    prefix='/auth',
+    tags=['auth'],
 )
+
+T_OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+T_Session = Annotated[Session, Depends(get_session)]
+T_User = Annotated[User, Depends(get_current_user)]
+
 
 @router.post('/token', response_model=Token)
 def login_for_access_token(
-        form_data: OAuth2PasswordRequestForm = Depends(),
-        session: Session = Depends(get_session),
+    form_data: T_OAuth2Form,
+    session: T_Session,
 ):
     user = session.scalar(select(User).where(User.email == form_data.username))
 
